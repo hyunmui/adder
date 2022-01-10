02ad305a49fd44419d2e6996ce30c17ad53e9944
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-01-10 22:08:13 +0900

    chapter 11

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,104 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!(
+                "반드시 1보다 크거나 같은 값을 사용해야 합니다. 지정된 값: {}",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "반드시 100보다 작거나 같은 값을 사용해야 합니다. 지정된 값: {}",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn exploration() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
-    fn another() {
-        panic!("Test Failed!");
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carroll");
+        assert!(
+            result.contains("Carroll"),
+            "Greeting 함수의 결과에 이름이 포함되어 있지 않음. 결과값: '{}'",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "반드시 100보다 작거나 같은 값을 사용해야 합니다.")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
